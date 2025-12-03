1d56f5d85f7a8e29f727fcb1330c0f02d01d0816
Froxayn123 <rizkymyusuf123@gmail.com>
2025-12-03 07:47:43 +0700

    feat: Adding Add Product & List Product

---
+++ models/Product.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    offerPrice: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: Array,
+      required: true,
+    },
+    date: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product =
+  mongoose.models.Product || mongoose.model("Product", productSchema);
+
+export default Product;
