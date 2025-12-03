68ccba9c7142010e88a16d5354d0b14ede5f5d50
Froxayn123 <rizkymyusuf123@gmail.com>
2025-12-03 07:49:02 +0700

    feat: Adding Create Order Feature

---
+++ app/api/order/create/route.js
@@ -0,0 +1,41 @@
+import { inngest } from "@/configs/inngest";
+import Product from "@/models/Product";
+import { getAuth, User } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  try {
+    const { userId } = getAuth(request);
+    const { address, items } = await request.json();
+    if (!address || !items) {
+      return NextResponse.json({ success: false, message: "Invalid Data" });
+    }
+    const amount = await items.reduce(async (acc, item) => {
+      const product = await Product.findById(item.product);
+      return acc + product.offerPrice * item.quantity;
+    }, 0);
+
+    await inngest.send({
+      name: "create-user-order",
+      data: {
+        userId,
+        address,
+        items,
+        amount: amount + Math.floor(amount * 0.02),
+        status: "Order Placed",
+        date: Date.now(),
+      },
+    });
+
+    const user = await User.findById(userId);
+    user.cartItems = {};
+    await user.save();
+
+    return NextResponse.json({
+      success: true,
+      message: "Order Placed",
+    });
+  } catch (error) {
+    return NextResponse.json({ success: false, message: error.message });
+  }
+}
